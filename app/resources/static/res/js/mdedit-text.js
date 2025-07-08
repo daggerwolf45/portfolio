df00bc63c7a28102451446bb34b65b0ae580f1ba
Sam Laird <me@slaird.dev>
2025-07-08 17:06:48 -0400

    feat: Proper wrapping support. Italics

--- app/resources/static/res/js/mdedit-text.js
+++ app/resources/static/res/js/mdedit-text.js
@@ -4,20 +4,91 @@ let history_len = 1000;
 const history = [];
 let undo = [];
 
+const fUndo = function(e){
+    if (history.length > 1) {
+        textarea.value = history.pop()
+        undo.push(textarea.value)
+    }
+}
+
+const fRedo = function(e){
+    if (undo.length > 1) {
+        textarea.value = undo.pop()
+    }
+}
+
+const _wrap = function(field, open, close){
+    const text = field.value;
+    const start = field.selectionStart;
+    const end = field.selectionEnd;
+
+    const dist = end - start;
+
+    field.value =
+        text.substring(0, start) +
+            open +
+            text.substring(start, start+dist) +
+            close +
+        text.substring(end);
+}
+
+const wrap = function(field, val){
+    return _wrap(field, val, val);
+}
+
+const wrapTag = function(field, val){
+    const open = `<${val}>`
+    const close = `</${val}>`
+
+    return _wrap(field, open, close);
+}
+
+const fBold = function(e){
+    wrap(textarea, '**');
+}
+
+const fItalic = function(e){
+    wrap(textarea, '*');
+}
+
+const fMono = function(e){
+    wrapTag(textarea, 'code');
+}
+
+const fTabnate = function(e){
+    const start = textarea.selectionStart;
+    const end = textarea.selectionEnd;
+    const text = textarea.value;
+
+    const tab = " ".repeat(tab_size);
+
+    textarea.value =
+        text.substring(0, start) +
+        tab +
+        text.substring(end);
+
+    textarea.selectionStart =
+        textarea.selectionEnd = start + tab_size;
+}
+
 textarea.addEventListener('keydown', (e) => {
     // Modifier functions
     if (e.ctrlKey) {
+        console.log(e.key)
         switch (e.key){
             case 'KeyZ':
-                if (history.length > 1) {
-                    textarea.value = history.pop()
-                    undo.push(textarea.value)
-                }
+                fUndo(e)
                 break;
             case 'KeyY':
-                if (undo.length > 1) {
-                    textarea.value = undo.pop()
-                }
+                fRedo(e)
+                break;
+            case 'b':
+                e.preventDefault();
+                fBold(e)
+                break;
+            case 'i':
+                e.preventDefault();
+                fItalic(e)
                 break;
             case 'Enter':
                 ReloadMarkdown().then()
@@ -29,19 +100,7 @@ textarea.addEventListener('keydown', (e) => {
         switch (e.key) {
             case 'Tab':
                 e.preventDefault();
-                const start = textarea.selectionStart;
-                const end = textarea.selectionEnd;
-                const text = textarea.value;
-
-                const tab = " ".repeat(tab_size);
-
-                textarea.value =
-                    text.substring(0, start) +
-                    tab +
-                    text.substring(end);
-
-                textarea.selectionStart =
-                    textarea.selectionEnd = start + tab_size;
+                fTabnate(e)
                 break;
             case 'Escape':
                 textarea.blur()
